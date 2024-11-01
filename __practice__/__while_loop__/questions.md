60c0ef8923d4390eaf63ae6b9a94a8b640458b6f
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-11-01 16:13:52 +0530

    p2 Done

--- __practice__/__while_loop__/questions.md
+++ __practice__/__while_loop__/questions.md
@@ -1,7 +1,7 @@
 # Questions
 
 ```
-program_1. Write a program to print first 10 even number using while loop take input from user
+program_1. Write a program to print first 10 even number using while loop take input from user.
 
 program_2. Write a program to print the first 10 integers and their squares using a while loop.
  
---
+++ __practice__/__while_loop__/solutions/program_2.py
@@ -0,0 +1,14 @@
+#program_2. Write a program to print the first 10 integers and their squares using a while loop.
+
+num= int(input("Enter value to get 10 integer number with their square: "))
+count= 0
+
+while count < 10:
+    
+    if num:
+        print("Square of integer value ",num," is ",num**2)
+        
+        count+=1
+    
+    num+=1
+
