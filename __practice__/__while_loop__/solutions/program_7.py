a3bd38d81a72724ae1c5c9d4f232c4c06cace67c
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-11-02 13:02:08 +0530

    Done

--- __practice__/__while_loop__/solutions/program_7.py
+++ __practice__/__while_loop__/solutions/program_7.py
@@ -1,25 +1,26 @@
 #program_7. Write a program to print the sum of the first 10 Even numbers.
 
 num= int(input("Enter the value to see the sum of the first 10 Even numbers: "))
-count= 0
-out= 0
-i= 1
+count= 0    #to store counting
+out= 0      #to store value of sum
+i= 1 
 
-while count<10:
+while count<10: #for make sure sum of 10 even number
     
     print("Now value of i for condition is:",i ,end=" ---> ")
     
     if i%2==0:
         
-        out+=i
+        out+=i      #add the current even value of i to out
         
-        count+=1
+        count+=1    #increment count by 1
+
         print("Count after getting",i ,"as a even number : ",count,"\n")
     
     else:
         print(i,"is an odd number")
     
-    i+=1
+    i+=1            #increment i by 1
 
     
 
