4520dc9d8888e2b3690db716f51510f774baa3bc
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-11-02 11:15:44 +0530

    Done

---
+++ __practice__/__while_loop__/solutions/program_5.py
@@ -0,0 +1,11 @@
+#program_5. Write a program to print the first 10 natural numbers in reverse order using a while loop.
+
+num= 10
+print("First 10 natural numbers in reverse order are: ")
+
+while num>0:
+    
+    print(num)
+
+    num-=1 #decrementing by 1
+
