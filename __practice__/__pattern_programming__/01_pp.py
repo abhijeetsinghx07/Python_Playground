68fe43dffa432dfca64664fbbac519ce618dccfb
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-11-04 15:05:18 +0530

    Done

--- __practice__/__pattern_programming__/01_pp.py
+++ __practice__/__pattern_programming__/01_pp.py
@@ -1,1 +1,6 @@
+#Print vertical stars pattern
 
+num= int(input("Enter the number of stars you want: "))
+
+for i in range(num):
+    print('*')
---
+++ __practice__/__while_loop__/solutions/program_14.py
@@ -0,0 +1,1 @@
+#program_14. Write a program to display the number names of the digits of a number entered by user, for example if the number is 231 then output should be Two Three One
