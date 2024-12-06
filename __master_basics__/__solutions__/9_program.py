fe5372d6a6e274ed2aa5daa254e71cf50d9c8684
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-12-06 23:41:16 +0530

    Update 9_program.py

--- __master_basics__/__solutions__/9_program.py
+++ __master_basics__/__solutions__/9_program.py
@@ -5,3 +5,12 @@ exam_st_date = (11, 12, 2014)
 Sample Output : The examination will start from : 11 / 12 / 2014
 '''
 
+
+exam_st_date = (11, 12, 2014)
+
+formatted_date = f"{exam_st_date[0]} / {exam_st_date[1]} / {exam_st_date[2]}"
+
+print(f"The examination will start from : {formatted_date}")
+
+
+
