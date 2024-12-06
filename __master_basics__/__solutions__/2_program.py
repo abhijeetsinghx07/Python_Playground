77496d3cd83547599377d189c0ed5aff23733628
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-12-06 23:33:02 +0530

    Update 2_program.py

--- __master_basics__/__solutions__/2_program.py
+++ __master_basics__/__solutions__/2_program.py
@@ -1,2 +1,15 @@
 #Write a program to find out what version of Python you are using.
 
+# print(sys.version)
+# print(sys.platform) 
+# print(sys.version_info) 
+
+import sys
+import platform
+
+# print(sys.version)
+# print(sys.platform) 
+# print(sys.version_info) 
+print(f"Python Version: {platform.python_version()}")
+print(f"Version Info: {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}")
+print(f"Python Implementation: {platform.python_implementation()}")
