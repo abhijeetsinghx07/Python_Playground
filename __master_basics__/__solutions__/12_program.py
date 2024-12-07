92d13e9fcb56073189ee1844f7b15c9daf154e50
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-12-07 22:16:02 +0530

    Update 12_program.py

--- __master_basics__/__solutions__/12_program.py
+++ __master_basics__/__solutions__/12_program.py
@@ -1,5 +1,6 @@
 #12. Monthly Calendar Display
+'''
+Write a program that prints the calendar for a given month and year.
 
-# Write a program that prints the calendar for a given month and year.
-
-# Note : Use 'calendar' module.
+Note : Use 'calendar' module.
+'''
