64e69fad38539ce68f5d1abc41c5ec954aa540a5
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-12-07 22:17:24 +0530

    Update 14_program.py

--- __master_basics__/__solutions__/14_program.py
+++ __master_basics__/__solutions__/14_program.py
@@ -2,6 +2,8 @@
 
 # Write a program to calculate the number of days between two dates.
 
-# Sample dates : (2014, 7, 2), (2014, 7, 11)
+'''
+Sample dates : (2014, 7, 2), (2014, 7, 11)
 
-# Expected output : 9 days
+Expected output : 9 days
+'''
