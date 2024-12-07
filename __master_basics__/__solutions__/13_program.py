f4bcec242ee3b4f42bd80839a42ca2146fc0dc08
Abhijeet Singh <108348003+abhijeetsinghx07@users.noreply.github.com>
2024-12-07 22:17:07 +0530

    Update 13_program.py

--- __master_basics__/__solutions__/13_program.py
+++ __master_basics__/__solutions__/13_program.py
@@ -2,8 +2,10 @@
 
 # Write a program to print the following 'here document'.
 
-# Sample string :
-# a string that you "don't" have to escape
-# This
-# is a ....... multi-line
-# heredoc string --------> example
+'''
+Sample string :
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+'''
