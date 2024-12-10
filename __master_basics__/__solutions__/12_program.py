70a1da1ce12e57a77f564ce7386f3c433a97b1bf
Abhijeet Singh <abhijeetsinghx07@users.noreply.github.com>
2024-12-10 04:51:57 -0800

    Update 12_program.py

--- __master_basics__/__solutions__/12_program.py
+++ __master_basics__/__solutions__/12_program.py
@@ -1,6 +1,12 @@
-#12. Monthly Calendar Display
-'''
-Write a program that prints the calendar for a given month and year.
+#12. Write a program that prints the calendar for a given month and year.
 
-Note : Use 'calendar' module.
-'''
+#Note : Use 'calendar' module.
+
+import calendar 
+
+month= int(input("Enter the month(1-12) to display calender: "))
+year=  int(input("Enter the year to display calender: "))
+
+print(calendar.month(year,month))
+
+
