ff7378a273f75b2d54cd29ad78590ba263cfa0ef
Abhijeet Singh <abhijeetsinghx07@users.noreply.github.com>
2024-12-10 05:09:05 -0800

    Update 14_program.py

--- __master_basics__/__solutions__/14_program.py
+++ __master_basics__/__solutions__/14_program.py
@@ -1,9 +1,16 @@
-# 14. Days Between Dates
-
-# Write a program to calculate the number of days between two dates.
+# 14. Write a program to calculate the number of days between two dates.
 
 '''
 Sample dates : (2014, 7, 2), (2014, 7, 11)
 
 Expected output : 9 days
 '''
+from datetime import date
+
+first_date = date(2014, 7, 2)
+last_date  = date(2014, 7, 11)
+
+
+diff = last_date - first_date
+
+print(f"Number of days between {first_date} & {last_date} is : {diff.days} days")
