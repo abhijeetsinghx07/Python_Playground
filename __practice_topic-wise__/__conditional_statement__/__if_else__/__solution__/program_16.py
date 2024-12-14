626d3f7bb992d9813271f3319cf2646cba2d4edd
Abhijeet Singh <abhijeetsinghx07@users.noreply.github.com>
2024-12-14 13:56:52 +0530

    Update program_16.py

--- __practice_topic-wise__/__conditional_statement__/__if_else__/__solution__/program_16.py
+++ __practice_topic-wise__/__conditional_statement__/__if_else__/__solution__/program_16.py
@@ -1,1 +1,16 @@
 #Write a program to check whether the year is leap year or not.
+'''
+Explanation of Leap Year Rules
+A year is a leap year if:
+
+- It is divisible by 400, OR
+- It is divisible by 4 but not divisible by 100.
+'''
+
+year = int(input("Enter the year to check: "))
+
+if year%400==0 or (year%4==0 and year%100!=0) :
+    print(f"{year} is leap year.")
+
+else:
+    print(f"{year} is not leap year.")
