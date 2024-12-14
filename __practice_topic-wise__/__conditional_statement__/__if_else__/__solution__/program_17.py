91141fe57e91c863b84b71e113adb63f5fbfd553
Abhijeet Singh <abhijeetsinghx07@users.noreply.github.com>
2024-12-14 15:15:39 +0530

    Update program_17.py

--- __practice_topic-wise__/__conditional_statement__/__if_else__/__solution__/program_17.py
+++ __practice_topic-wise__/__conditional_statement__/__if_else__/__solution__/program_17.py
@@ -1,1 +1,10 @@
 #Write a program to check whether a number entered is a 3 digit number or not.
+#Without using type casting.
+
+num= int(input("Enter 3 digit number: "))
+
+if 100 <= num <=999 or -999 <= num <= -100  :
+    print(f"{num} is a three digit number.")
+
+else:
+    print(f"{num} is not three digit number.")
