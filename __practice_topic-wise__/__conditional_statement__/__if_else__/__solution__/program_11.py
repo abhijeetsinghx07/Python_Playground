e97622b9e048407a1b57d6a5564ce5a92e75fb70
Abhijeet Singh <abhijeetsinghx07@users.noreply.github.com>
2024-12-14 14:00:35 +0530

    Update program_11.py

--- __practice_topic-wise__/__conditional_statement__/__if_else__/__solution__/program_11.py
+++ __practice_topic-wise__/__conditional_statement__/__if_else__/__solution__/program_11.py
@@ -1,8 +1,9 @@
 #Write a program to check whether the given string is palindrome or not.
+#A string is said to be palindrome if it remains the same on reading from both ends.
 
-inp= input("Enter a strind to check: ")
+inp= input("Enter a string to check if it's a palindrome: ")
 
-if inp[0:] == inp[::-1]:
+if inp == inp[::-1]:
     print("It is palindrome.")
 
 else:
