50dc2b70afbc5c7f95254ada919469068d470a57
Abhijeet Singh <abhijeetsinghx07@users.noreply.github.com>
2025-01-06 22:14:39 +0530

    update

--- __master_basics__/__solutions__/program_31.py
+++ __master_basics__/__solutions__/program_31.py
@@ -9,3 +9,9 @@ for i in range(2,num_1+num_2):
         num_div.append(i)
 
 print(f"Greatest Common divisor of {num_1} and {num_2} is: {num_div}")
+
+for j in range(len(num_div)):
+    if num_div[j]>num_div[j+1] and num_div[j+1]>num_div[j+2]:
+        print(j)
+    elif 
+
--- __master_basics__/__solutions__/program_33.py
+++ __master_basics__/__solutions__/program_33.py
@@ -1,3 +1,12 @@
 # 33. Write a program to sum three given integers. However, if two values are equal, the sum will be zero.
 
 
+num_1 = int(input("Enter the first value: "))
+num_2 = int(input("Enter the second value: "))
+num_3 = int(input("Enter the third value: "))
+
+if num_1!=num_2:
+    print(f"Sum of {num_1} and {num_2} is: {num_1+num_2+num_3} ")
+
+else:
+    print(f"Sum of {num_1} {num_2} and {num_3} is: 0 ")
